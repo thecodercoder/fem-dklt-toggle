58207b57a5792e55fd5265e7a5d0d4b2d7c75d8c
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-07-08 21:10:54 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// Initialize modules
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Use dart-sass for @use
+//sass.compiler = require('dart-sass');
+
+// Sass Task
+function scssTask() {
+	return src('app/scss/style.scss', { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask() {
+	return src('app/js/script.js', { sourcemaps: true })
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.pipe(terser())
+		.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync
+function browserSyncServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: '.',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+	cb();
+}
+function browserSyncReload(cb) {
+	browsersync.reload();
+	cb();
+}
+
+// Watch Task
+function watchTask() {
+	watch('*.html', browserSyncReload);
+	watch(
+		['app/scss/**/*.scss', 'app/**/*.js'],
+		series(scssTask, jsTask, browserSyncReload)
+	);
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
---
+++ notes.md
@@ -0,0 +1,18 @@
+# Functional Requirements and Notes
+
+Light/Dark Mode toggle -- takes system pref by default, but can override with toggle
+
+What HTML markup (accessible) -- https://scottaohara.github.io/a11y_styled_form_controls/src/radio-button--switch/
+
+Use fieldset, legend, radio inputs
+
+Switching between light/dark modes via JS and Prefers-color-scheme media query -- https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
+
+Three option toggle: light/dark/system pref -- https://codepen.io/renddrew/pen/bRomab?editors=1100
+
+CSS Variables (custom properties) -- https://css-tricks.com/updating-a-css-variable-with-javascript/
+
+Accessibility
+
+- Use correct heading tags
+- Screenreader-only text for card titles/username -- https://www.accessibility-developer-guide.com/examples/hiding-elements/visually/
