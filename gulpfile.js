a2564102850fc63aefd4702b61ddcea7c00d9d60
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-09-01 14:42:14 -0500

    add gulpfile, scss tweak

--- gulpfile.js
+++ gulpfile.js
@@ -13,48 +13,51 @@ const browsersync = require('browser-sync').create();
 
 // Sass Task
 function scssTask() {
-	return src('app/scss/style.scss', { sourcemaps: true })
-		.pipe(sass())
-		.pipe(postcss([autoprefixer(), cssnano()]))
-		.pipe(dest('dist', { sourcemaps: '.' }));
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // JavaScript Task
 function jsTask() {
-	return src('app/js/script.js', { sourcemaps: true })
-		.pipe(babel({ presets: ['@babel/preset-env'] }))
-		.pipe(terser())
-		.pipe(dest('dist', { sourcemaps: '.' }));
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // Browsersync
 function browserSyncServe(cb) {
-	browsersync.init({
-		server: {
-			baseDir: '.',
-		},
-		notify: {
-			styles: {
-				top: 'auto',
-				bottom: '0',
-			},
-		},
-	});
-	cb();
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  cb();
 }
 function browserSyncReload(cb) {
-	browsersync.reload();
-	cb();
+  browsersync.reload();
+  cb();
 }
 
 // Watch Task
 function watchTask() {
-	watch('*.html', browserSyncReload);
-	watch(
-		['app/scss/**/*.scss', 'app/**/*.js'],
-		series(scssTask, jsTask, browserSyncReload)
-	);
+  watch('*.html', browserSyncReload);
+  watch(
+    ['app/scss/**/*.scss', 'app/**/*.js'],
+    series(scssTask, jsTask, browserSyncReload)
+  );
 }
 
 // Default Gulp Task
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask, jsTask);
